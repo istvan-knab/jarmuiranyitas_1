d6393aa97dfd3c4fbd0bc6e759b02ba830f9ab10
BPelenczei <balint@pelenczei.hu>
2022-10-25 14:05:10 +0200

    CHANGE load function of MatlabMapLoader class (exclude rows 2,3)

--- misc/load_map.py
+++ misc/load_map.py
@@ -5,11 +5,11 @@ from abc import ABC, abstractmethod
 
 class MapLoader(ABC):
     @abstractmethod
-    def load(self, file_name) -> np.ndarray:
+    def load(self, file_name: str) -> np.ndarray:
         """
-
-        :param file_name:
-        :return:
+        This function creates a standard numpy array from the map data given as an argument.
+        :param file_name: name of the map file to be preprocessed, excluding extension
+        :return: numpy array of size (number_of_coordinates, 2) where each row corresponds to an (x, y) coordinate pair
         """
         pass
 
@@ -18,6 +18,6 @@ class MatlabMapLoader(MapLoader):
     def load(self, file_name) -> np.ndarray:
         map_data = scipy.io.loadmat("maps/" + file_name + '.mat')
         map_data = map_data['xyz']
-        map_data = np.delete(map_data, 2, 1)
+        map_data = np.delete(map_data, (2, 3), 1)
 
         return map_data
