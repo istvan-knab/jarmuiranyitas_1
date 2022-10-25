620debc520f06010e090973260354a5429f74546
BPelenczei <balint@pelenczei.hu>
2022-10-26 00:21:02 +0200

    CHANGE implementation details in DQNAgentWithPER class

--- agents/dqn_agent_with_per.py
+++ agents/dqn_agent_with_per.py
@@ -8,12 +8,9 @@ import torch.optim as optimizer
 
 from jarmuiranyitas_1.agents.agent import Agent
 from jarmuiranyitas_1.agents.misc.per_memory import PERMemory
-from jarmuiranyitas_1.agents.misc.per_memory import EGreedy
-from jarmuiranyitas_1.agents.misc.per_memory import NNInitializer
-
-torch.manual_seed(0)
-random.seed(0)
-np.random.seed(0)
+from jarmuiranyitas_1.agents.misc.er_memory import ERMemory
+from jarmuiranyitas_1.agents.misc.e_greedy import EGreedy
+from jarmuiranyitas_1.agents.misc.neural_network import NNInitializer
 
 
 class DQNAgentWithPER(Agent):
@@ -22,25 +19,29 @@ class DQNAgentWithPER(Agent):
                  discount_factor: float = 0.99,
                  batch_size: int = 128,
                  network_size: list = None,
-                 network_type: str = "mlp",
+                 network_type: str = "cnn",
                  seed: int = 0,
                  epsilon_start: float = 1,
                  epsilon_decay: float = 0.99,
-                 memory_size: int = 2e20,
+                 memory_size: int = 1e20,
                  memory_alpha: float = 0.6,
                  memory_beta: float = 0.4,
                  memory_beta_increment: float = 0.0005,
-                 memory_epsilon: float = 0.01) -> None:
+                 memory_epsilon: float = 0.01,
+                 per: bool = False) -> None:
         self.learning_rate = learning_rate
         self.discount_factor = discount_factor
         self.batch_size = batch_size
-        self.memory = PERMemory(memory_size, memory_alpha, memory_beta, memory_beta_increment, memory_epsilon)
-        self.e_greedy = EGreedy(epsilon_start, epsilon_decay)
+        if per:
+            self.memory = PERMemory(memory_size, memory_alpha, memory_beta, memory_beta_increment, memory_epsilon)
+        else:
+            self.memory = ERMemory(memory_size)
         self.action_network = NNInitializer(network_type, network_size)
         self.target_network = NNInitializer(network_type, network_size)
+        self.e_greedy = EGreedy(epsilon_start, epsilon_decay, self.action_network)
         self.seed = seed
 
-    def seed(self) -> None:
+    def seed(self, seed) -> None:
         pass
 
     def inference(self, state: np.ndarray) -> np.ndarray:
