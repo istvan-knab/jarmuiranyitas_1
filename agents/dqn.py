e066e75ea3005b3b9ebd56e2f92e5361374f291c
BPelenczei <balint@pelenczei.hu>
2022-10-25 20:09:50 +0200

    ADD imports of dqn agent components

--- agents/dqn.py
+++ agents/dqn.py
@@ -6,6 +6,11 @@ import torch
 import torch.nn as nn
 import torch.optim as optimizer
 
+from jarmuiranyitas_1.agents.agent import Agent
+from jarmuiranyitas_1.agents.misc.per_memory import PERMemory
+from jarmuiranyitas_1.agents.misc.per_memory import EGreedy
+from jarmuiranyitas_1.agents.misc.per_memory import NNInitializer
+
 torch.manual_seed(0)
 random.seed(0)
 np.random.seed(0)
