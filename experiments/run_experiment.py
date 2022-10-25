f77ff4db7d0742a4efdae7d7c9a5d5bfd034d366
BPelenczei <balint@pelenczei.hu>
2022-10-26 00:24:38 +0200

    ADD first layer user interface run_experiment.py

---
+++ experiments/run_experiment.py
@@ -0,0 +1,25 @@
+from argparse import ArgumentParser
+
+from jarmuiranyitas_1.experiments.trainer import Trainer
+from jarmuiranyitas_1.experiments.agent_initializer import AgentInitializer
+from jarmuiranyitas_1.experiments.environment_initializer import EnvironmentInitializer
+
+
+def main():
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument("--seed", default=None, type=int)
+    arg_parser.add_argument("--map_name", default='xyz_palya', type=str)
+    arg_parser.add_argument("--training_name", default='Training_results', type=str)
+    arg_parser.add_argument("--agent", default='dqn', type=str)
+    arg_parser.add_argument("--max_episodes", default=1e6, type=int)
+    args = arg_parser.parse_args()
+
+    env_initializer = EnvironmentInitializer(args.seed, args.map_name)
+    agent_initializer = AgentInitializer(args.seed, args.agent)
+    trainer = Trainer(args.training_name, env_initializer.env, agent_initializer.agent, args.max_episodes)
+
+    trainer.train()
+
+
+if __name__ == "__main__":
+    main()
