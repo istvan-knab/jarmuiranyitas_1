7e94fd2b137b2c8840b3da1b0945d53a0d08cd07
BPelenczei <balint@pelenczei.hu>
2022-10-25 20:13:06 +0200

    REMOVE initial parameter setup in dqn agent definition file

--- agents/dqn.py
+++ agents/dqn.py
@@ -15,15 +15,8 @@ torch.manual_seed(0)
 random.seed(0)
 np.random.seed(0)
 
-LEARNING_RATE = 1e-4
-GAMMA = 0.99
-MEMORY_SIZE = 2e20
-EPSILON_START = 1
-EPSILON_DECAY = 0.99
-EPSILON_MEMORY = 0.01
-BATCH_SIZE = 128
-ALPHA = 0.6
-BETA = 0.4
-BETA_INCREMENT = 0.01
-NN_SIZES = [1, 1, 1, 1]
-NN_TYPE = "cnn"             # cnn or mlp
+    def inference(self, state: np.ndarray) -> np.ndarray:
+        pass
+
+    def fit(self) -> None:
+        pass
