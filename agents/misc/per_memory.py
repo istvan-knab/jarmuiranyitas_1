ec474bf751c53fb9d562006526b1a4e1995219ba
BPelenczei <balint@pelenczei.hu>
2022-10-25 20:09:06 +0200

    ADD init arguments to PERMemory constructor

--- agents/misc/per_memory.py
+++ agents/misc/per_memory.py
@@ -5,7 +5,7 @@ from jarmuiranyitas_1.agents.misc.memory import Memory
 
 class PERMemory(Memory):
 
-    def __init__(self, size: int) -> None:
+    def __init__(self, size: int, alpha, beta, beta_increment, epsilon) -> None:
         pass
 
     def save(self, experience: np.ndarray) -> None:
