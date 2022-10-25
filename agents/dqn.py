18fc33fac6ec55778046377c3f493d19fef8a163
BPelenczei <balint@pelenczei.hu>
2022-10-25 20:13:54 +0200

    ADD implementation of DQNAgentWithPER class constructor

--- agents/dqn.py
+++ agents/dqn.py
@@ -15,6 +15,34 @@ torch.manual_seed(0)
 random.seed(0)
 np.random.seed(0)
 
+
+class DQNAgentWithPER(Agent):
+    def __init__(self,
+                 learning_rate: float = 1e-4,
+                 discount_factor: float = 0.99,
+                 batch_size: int = 128,
+                 network_size: list = None,
+                 network_type: str = "mlp",
+                 seed: int = 0,
+                 epsilon_start: float = 1,
+                 epsilon_decay: float = 0.99,
+                 memory_size: int = 2e20,
+                 memory_alpha: float = 0.6,
+                 memory_beta: float = 0.4,
+                 memory_beta_increment: float = 0.0005,
+                 memory_epsilon: float = 0.01) -> None:
+        self.learning_rate = learning_rate
+        self.discount_factor = discount_factor
+        self.batch_size = batch_size
+        self.memory = PERMemory(memory_size, memory_alpha, memory_beta, memory_beta_increment, memory_epsilon)
+        self.e_greedy = EGreedy(epsilon_start, epsilon_decay)
+        self.action_network = NNInitializer(network_type, network_size)
+        self.target_network = NNInitializer(network_type, network_size)
+        self.seed = seed
+
+    def seed(self) -> None:
+        pass
+
     def inference(self, state: np.ndarray) -> np.ndarray:
         pass
 
