b3c7ccf9220d4e25d5a542b570f6a0423d208ac2
isti_knab <istvan.knab@edu.bme.hu>
2022-10-26 08:55:39 +0200

    ADD FILE: initialize the structure of saving files

--- experiments/trainer.py
+++ experiments/trainer.py
@@ -1,6 +1,12 @@
 class Trainer:
     def __init__(self, name, env, agent, max_episodes):
-        pass
+        self.MAX_EPISODE = max_episodes
 
     def train(self):
-        pass
+        """
+        Training loop
+        Returns:
+
+        """
+        for episode in range(self.EPISODE):
+            pass
