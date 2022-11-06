cffe7b395726a8aa9c95da3e39e9ad750ebafd3e
istvan-knab <97619115+istvan-knab@users.noreply.github.com>
2022-11-06 21:14:21 +0100

    create optimizer skeleton

---
+++ agents/misc/optimizer.py
@@ -0,0 +1,17 @@
+import torch
+import numpy
+
+
+class Optimizer:
+
+    def __init__(self):
+        pass
+
+    def get_sample(self):
+        pass
+
+    def calc_q_value(self):
+        pass
+
+    def optimize(self):
+        pass
