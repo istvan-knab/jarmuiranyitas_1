03a8434a0c0df0e5402d644978825136845dfce4
BPelenczei <81440132+BPelenczei@users.noreply.github.com>
2022-11-06 13:17:10 +0100

    ADD implementation of create_env function in environment_initializer.py

--- experiments/environment_initializer.py
+++ experiments/environment_initializer.py
@@ -13,5 +13,7 @@ class EnvironmentInitializer:
     def env(self):
         return self._env
 
-    def create_env(self):
-        self._env = gym.make('f110_gym:f110-v0')
+    def _create_env(self, seed, map_name, map_ext):
+        path = os.path.abspath("../misc/maps")
+        path = ''.join([path, "/", map_name])
+        self._env = gym.make('f110_gym:f110-v0', seed=seed, map=path, map_ext=map_ext, num_agents=1)
