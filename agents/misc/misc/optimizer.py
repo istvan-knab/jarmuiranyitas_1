4a9ab1319faeeed4a8dd3fb48930a5242715acce
istvan-knab <97619115+istvan-knab@users.noreply.github.com>
2022-11-08 00:50:02 +0100

    ADD FEATURE : Implement constructor and sample in optimizer.py

--- agents/misc/misc/optimizer.py
+++ agents/misc/misc/optimizer.py
@@ -14,16 +14,23 @@ class Optimizer:
     """
 
     MEMORY_ITEM = namedtuple('Transition', ('state', 'action', 'next_state', 'reward', 'done'))
-    def __init__(self, memory,BATCH_SIZE):
+    def __init__(self, memory, BATCH_SIZE):
         self.memory = memory
         self.BATCH_SIZE = BATCH_SIZE
+        self.size = min(self.batch_size, len(self.memory))
 
     def get_sample(self):
-        pass
+        sample = self.memory.sample(self.size)
+        mini_batch = self.memory.MEMORY_ITEM(*zip(*sample))
+
+        state_batch = torch.FloatTensor(mini_batch.state)
+        action_batch = torch.reshape(torch.LongTensor(mini_batch.action), (self.batch_size, 1))
+        next_state_batch = torch.FloatTensor(mini_batch.next_state)
+        reward_batch = torch.reshape(torch.FloatTensor(mini_batch.reward), (self.batch_size, 1))
 
     def calc_q_value(self):
         pass
 
-    def optimize(self):
+    def optimizer_step(self):
         if len(self.memory) < self.BATCH_SIZE:
             return 0
