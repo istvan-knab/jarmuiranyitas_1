802b9e0a1e7c055d6fa2b180c24e0ad31875aa31
istvan-knab <97619115+istvan-knab@users.noreply.github.com>
2022-11-10 14:42:54 +0100

    ADD FEATURE: already existing yaml , almost perfect

--- experiments/init/environment_initializer.py
+++ experiments/init/environment_initializer.py
@@ -1,7 +1,8 @@
 from jarmuiranyitas_1.f110_env.f110_gym.envs.f110_env import F110Env
 import os
 from abc import abstractmethod
-
+import yaml
+from argparse import Namespace
 
 class EnvironmentInitializer:
 
@@ -24,6 +25,11 @@ class EnvironmentInitializer:
         pass
 
     def _create_env_f110(self, **kwargs):
+        config_path = os.path.abspath('init/environment_config.yaml')
+        with open(config_path) as file:
+            conf_dict = yaml.load(file, Loader=yaml.FullLoader)
+        self._config = Namespace(**conf_dict)
+
         path = os.path.abspath("../misc/maps")
         path = ''.join([path, "/", kwargs["map_name"]])
         self._env = F110Env(seed=kwargs["seed"], map=path, map_ext=kwargs["map_ext"], num_agents=1, timestep=0.01)
