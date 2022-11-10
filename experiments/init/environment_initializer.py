442217271c625727a5d5b6884ebaef065d4dc850
istvan-knab <97619115+istvan-knab@users.noreply.github.com>
2022-11-10 22:06:34 +0100

    config ... almost good , communication failed

--- experiments/init/environment_initializer.py
+++ experiments/init/environment_initializer.py
@@ -8,6 +8,7 @@ class EnvironmentInitializer:
 
     def __init__(self, env_name, **kwargs):
         self._env = None
+        self._load_environment_config()
 
         if env_name == 'f110':
             self._create_env = self._create_env_f110
@@ -24,13 +25,22 @@ class EnvironmentInitializer:
     def _create_env_abstract(self, **kwargs):
         pass
 
-    def _create_env_f110(self, **kwargs):
-        """
-        config_path = os.path.abspath('init/environment_config.yaml')
+    def _load_environment_config(self) -> None:
+        config_path = os.path.abspath('init/environment_config.yml')
         with open(config_path) as file:
             conf_dict = yaml.load(file, Loader=yaml.FullLoader)
         self._config = Namespace(**conf_dict)
-        """
+
+    def _create_env_f110(self, **kwargs):
+
+
         path = os.path.abspath("../misc/maps")
         path = ''.join([path, "/", kwargs["map_name"]])
+        '''
+        self._env =  F110Env(self._config.seed,
+                             self._config.path,
+                             self._config.map_ext,
+                             self._config.num_agents,
+                             self._config.time_step)
+        '''
         self._env = F110Env(seed=kwargs["seed"], map=path, map_ext=kwargs["map_ext"], num_agents=1, timestep=0.01)
