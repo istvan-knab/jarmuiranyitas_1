cb83e06f204b348f146672cc197dae142c55be71
BPelenczei <81440132+BPelenczei@users.noreply.github.com>
2022-11-21 20:13:59 +0100

    CHANGE update_epsilon location to outer scope

--- agents/dqn_agent_with_per.py
+++ agents/dqn_agent_with_per.py
@@ -103,8 +103,6 @@ class DQNAgentWithPER(Agent):
 
         self.optimizer.step()
 
-        self.e_greedy.update_epsilon()
-
     def update_networks(self):
         self.target_network.load_state_dict(OrderedDict(self.action_network.state_dict()))
 
